b964e6778bf5d8f22b9cb341809f4a59d019e450
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-09-02 00:33:24 -0500

    first time commiting

---
+++ main.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed(".text", {
+        strings: ["Student", "Developer", "Designer", "Artist"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true
+    });
+});
